9fe409a76661f719914a09588a23219113f1c721
Aya <lechhabiaya06@gmail.com>
2026-07-28 05:59:46 -0700

    Ajout de la base de données, des routes et des templates

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
+import pymysql
+import config
 
 app = Flask(__name__)
 
+def get_db_connection():
+    connection = pymysql.connect(
+        host=config.MYSQL_HOST,
+        user=config.MYSQL_USER,
+        password=config.MYSQL_PASSWORD,
+        database=config.MYSQL_DB
+    )
+    return connection
+
 @app.route('/')
-def home():
-    return "Mon site de restaurant fonctionne !"
+def accueil():
+    return render_template('index.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/reservation')
+def reservation():
+    return render_template('reservation.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
