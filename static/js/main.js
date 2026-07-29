1fb49fce9e96d5895533744c38fae725d6cb8193
Aya <lechhabiaya06@gmail.com>
2026-07-29 15:00:50 -0700

    Ajout du logo et des photos Bewok, personnalisation du nom du restaurant

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,14 @@
-console.log("main.js chargé avec succès");
+const btn = document.getElementById('lang');
+const langues = ['fr', 'en', 'es'];
+let indexLangue = 0;
+
+btn.addEventListener('click', function() {
+    indexLangue = (indexLangue + 1) % langues.length;
+    const langueCourrante = langues[indexLangue];
+    btn.textContent = langueCourrante.toUpperCase();
+
+    const elements = document.querySelectorAll('[data-fr]');
+    elements.forEach(function(el) {
+       el.innerHTML  = el.getAttribute('data-' + langueCourrante);
+    });
+});
